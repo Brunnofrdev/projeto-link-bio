384d6e8946a9669483ae6c153fcad17e5d11836a
Brunnofrdev <brunofrdev@gmail.com>
2025-03-22 12:00:21 -0300

    adjust javascript

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function toggleModel() {
-    const html = document.documentElement
-
-    html.classList.toggle("light")
-
-    const img = document.querySelector(".profile img")
-
-    if(html.classList.contains("light"))
-        img.setAttribute("src","./assets/branco.png") 
-
-    else {
-        img.setAttribute("src", "./assets/preto.png")
+document.addEventListener("DOMContentLoaded", () => {
+    const html = document.documentElement;
+    const img = document.querySelector(".profile img");
+  
+    // Verificar se já está no modo claro (light)
+    if (html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/branco.png");
+    } else {
+      img.setAttribute("src", "./assets/preto.png");
     }
-   
-    
-
-
-}
-
+  });
+  
+  function toggleModel() {
+    const html = document.documentElement;
+    html.classList.toggle("light");
+  
+    const img = document.querySelector(".profile img");
+  
+    // Trocar a imagem ao alternar entre os modos
+    if (html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/branco.png");
+    } else {
+      img.setAttribute("src", "./assets/preto.png");
+    }
+  }
+  
